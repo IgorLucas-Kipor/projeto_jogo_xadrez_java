085e44d7b75dfceb7b605b8360f0bfef22907f4e
Igor Lucas <igorescritor@hotmail.com>
2020-12-01 18:33:56 -0300

    Primeira classe: posição

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,11 +1,14 @@
 package aplicacao;
 
+import jogodetabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		Posicao pos = new Posicao(3, 5);
 		
-		System.out.println("Olá mundo.");
+		System.out.println(pos);
 
 	}
 
