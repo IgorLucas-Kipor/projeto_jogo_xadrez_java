a8181ebd2966932cee33fa0cb2129252f5c63d52
Igor Lucas <igorescritor@hotmail.com>
2020-12-01 18:44:39 -0300

    Começando a implementar Tabuleiro e Peça

---
+++ src/jogodetabuleiro/Peça.java
@@ -0,0 +1,21 @@
+package jogodetabuleiro;
+
+public class Peça {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peça(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+	
+	
+
+}
