4b8aafa762c2727981ad4fa95d9bd9cdc230840c
Igor Lucas <igorescritor@hotmail.com>
2020-12-24 22:14:31 -0300

    Implementando movimento especial: En Passant

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -21,6 +21,7 @@ public class PartidaXadrez {
 	private Cor jogadorAtual;
 	private boolean xeque;
 	private boolean xequeMate;
+	private PeçaXadrez vulneravelEnPassant;
 	
 	private List<Peça> peçasNoTabuleiro = new ArrayList<>();
 	private List<Peça> peçasCapturadas = new ArrayList<>();
@@ -48,6 +49,10 @@ public class PartidaXadrez {
 		return xequeMate;
 	}
 	
+	public PeçaXadrez getVulneravelEnPassant() {
+		return vulneravelEnPassant;
+	}
+	
 	public PeçaXadrez[][] pegarPeças() {
 		PeçaXadrez[][] mat = new PeçaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
 		for (int i=0; i<tabuleiro.getLinhas(); i++) {
@@ -71,6 +76,9 @@ public class PartidaXadrez {
 			throw new ChessException("You can't put yourself in check.");
 		}
 		
+		PeçaXadrez peçaMovida = (PeçaXadrez) tabuleiro.peça(destino);
+		
+		
 		xeque = (testarXeque(oponente(jogadorAtual))) ? true : false;
 		
 		if (testarXequeMate(oponente(jogadorAtual))) {
@@ -78,6 +86,16 @@ public class PartidaXadrez {
 		} else {
 			proximoTurno();
 		}
+		
+		// #movimento especial En Passant
+		
+		if (peçaMovida instanceof Peao && (destino.getLinha() == origem.getLinha()+2 || destino.getLinha() == origem.getLinha()-2)) {
+			vulneravelEnPassant = peçaMovida;
+		} else {
+			vulneravelEnPassant = null;
+		}
+		
+		
 		return (PeçaXadrez) peçaCapturada;
 	}
 	
@@ -133,6 +151,27 @@ public class PartidaXadrez {
 			tabuleiro.colocarPeça(torre, destinoT);
 			torre.aumentarContadorMovimentos();
 		}
+		
+		// #movimento especial En Passant
+		
+		if (p instanceof Peao) {
+			if (origem.getColuna() != destino.getColuna() && peçaCapturada == null) {
+				Posicao posicaoPeao;
+				if (p.getCor() == Cor.BRANCO) {
+					posicaoPeao = new Posicao (destino.getLinha()+1, destino.getColuna());
+					peçaCapturada = tabuleiro.removerPeça(posicaoPeao);
+					peçasCapturadas.add(peçaCapturada);
+					peçasNoTabuleiro.remove(peçaCapturada);
+				} else {
+					posicaoPeao = new Posicao (destino.getLinha()-1, destino.getColuna());
+					peçaCapturada = tabuleiro.removerPeça(posicaoPeao);
+					peçasCapturadas.add(peçaCapturada);
+					peçasNoTabuleiro.remove(peçaCapturada);
+				}
+			}
+		}
+		
+		
 		return peçaCapturada;
 	}
 	
@@ -165,6 +204,24 @@ public class PartidaXadrez {
 			tabuleiro.colocarPeça(torre, origemT);
 			torre.diminuirContadorMovimentos();
 		}
+		
+		// #movimento especial En Passant
+		
+		if (p instanceof Peao) {
+			if (origem.getColuna() != destino.getColuna() && peçaCapturada == vulneravelEnPassant) {
+				PeçaXadrez peao = (PeçaXadrez) tabuleiro.removerPeça(destino);
+				Posicao posicaoPeao;
+				if (p.getCor() == Cor.BRANCO) {
+					posicaoPeao = new Posicao (3, destino.getColuna());
+				} else {
+					posicaoPeao = new Posicao (4, destino.getColuna());
+				}
+				
+				tabuleiro.colocarPeça(peao, posicaoPeao);
+			}
+		}
+		
+		
 	}
 	
 	private Cor oponente(Cor cor) {
@@ -244,14 +301,14 @@ public class PartidaXadrez {
         colocarNovaPeça('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeça('h', 1, new Torre(tabuleiro, Cor.BRANCO));
         colocarNovaPeça('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('a', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('b', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('c', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('d', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('e', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('f', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('g', 2, new Peao(tabuleiro, Cor.BRANCO));
-        colocarNovaPeça('h', 2, new Peao(tabuleiro, Cor.BRANCO));
+        colocarNovaPeça('a', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('b', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('c', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('d', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('e', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('f', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('g', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+        colocarNovaPeça('h', 2, new Peao(tabuleiro, Cor.BRANCO, this));
 
         colocarNovaPeça('a', 8, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeça('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
@@ -261,14 +318,14 @@ public class PartidaXadrez {
         colocarNovaPeça('h', 8, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeça('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
         colocarNovaPeça('f', 8, new Bispo(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('a', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('b', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('c', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('d', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('e', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('f', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('g', 7, new Peao(tabuleiro, Cor.PRETO));
-        colocarNovaPeça('h', 7, new Peao(tabuleiro, Cor.PRETO));
+        colocarNovaPeça('a', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('b', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('c', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('d', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('e', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('f', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('g', 7, new Peao(tabuleiro, Cor.PRETO, this));
+        colocarNovaPeça('h', 7, new Peao(tabuleiro, Cor.PRETO, this));
 	}
 	
 
--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -3,12 +3,16 @@ package xadrez.pecas;
 import jogodetabuleiro.Posicao;
 import jogodetabuleiro.Tabuleiro;
 import xadrez.Cor;
+import xadrez.PartidaXadrez;
 import xadrez.PeçaXadrez;
 
 public class Peao extends PeçaXadrez {
+	
+	private PartidaXadrez partidaXadrez;
 
-	public Peao(Tabuleiro tabuleiro, Cor cor) {
+	public Peao(Tabuleiro tabuleiro, Cor cor, PartidaXadrez partidaXadrez) {
 		super(tabuleiro, cor);
+		this.partidaXadrez = partidaXadrez;
 	}
 
 	@Override
@@ -41,6 +45,22 @@ public class Peao extends PeçaXadrez {
 			if (getTabuleiro().posicaoExiste(p) && haUmaPeçaOponente(p)) {
 				matriz[p.getLinha()][p.getColuna()] = true;
 			}
+			
+			// #movimento especial En Passant BRANCO
+			
+			if (posicao.getLinha() == 3) {
+				Posicao esquerda = new Posicao (posicao.getLinha(), posicao.getColuna()-1);
+				if (getTabuleiro().posicaoExiste(esquerda) && haUmaPeçaOponente(esquerda) && getTabuleiro().peça(esquerda) == partidaXadrez.getVulneravelEnPassant()) {
+					matriz[esquerda.getLinha()-1][esquerda.getColuna()] = true;
+				}
+				
+				Posicao direita = new Posicao (posicao.getLinha(), posicao.getColuna()+1);
+				if (getTabuleiro().posicaoExiste(direita) && haUmaPeçaOponente(direita) && getTabuleiro().peça(direita) == partidaXadrez.getVulneravelEnPassant()) {
+					matriz[direita.getLinha()-1][direita.getColuna()] = true;
+				}
+			}
+			
+			
 		} else { // movimentos da peça afro-descendente
 			p.setValores(posicao.getLinha() + 1, posicao.getColuna());
 
@@ -63,6 +83,21 @@ public class Peao extends PeçaXadrez {
 			if (getTabuleiro().posicaoExiste(p) && haUmaPeçaOponente(p)) {
 				matriz[p.getLinha()][p.getColuna()] = true;
 			}
+			
+			// #movimento especial En Passant PRETO
+			
+			if (posicao.getLinha() == 4) {
+				Posicao esquerda = new Posicao (posicao.getLinha(), posicao.getColuna()-1);
+				if (getTabuleiro().posicaoExiste(esquerda) && haUmaPeçaOponente(esquerda) && getTabuleiro().peça(esquerda) == partidaXadrez.getVulneravelEnPassant()) {
+					matriz[esquerda.getLinha()+1][esquerda.getColuna()] = true;
+				}
+				
+				Posicao direita = new Posicao (posicao.getLinha(), posicao.getColuna()+1);
+				if (getTabuleiro().posicaoExiste(direita) && haUmaPeçaOponente(direita) && getTabuleiro().peça(direita) == partidaXadrez.getVulneravelEnPassant()) {
+					matriz[direita.getLinha()+1][direita.getColuna()] = true;
+				}
+			}
+			
 		}
 
 		return matriz;
